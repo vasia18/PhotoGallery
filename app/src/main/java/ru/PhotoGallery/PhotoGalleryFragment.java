2ba080bae79b8587414bc95b5fef071c4b208e9d
vasia18 <studiovasia18@gmail.com>
2021-01-31 21:26:56 +0300

    Добавлен код в AndroidManifest uses-permission android:name="android.permission.INTERNET"

--- app/src/main/java/ru/PhotoGallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/PhotoGallery/PhotoGalleryFragment.java
@@ -13,31 +13,31 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class PhotoGalleryFragment extends Fragment {
 
-    private RecyclerView mPhotoRecyclerView;
+	private RecyclerView mPhotoRecyclerView;
 
-    public static PhotoGalleryFragment newInstance() {
-        return new PhotoGalleryFragment();
-    }
-
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
-
-        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.Photo_recycler_view);
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+	public static PhotoGalleryFragment newInstance() {
+		return new PhotoGalleryFragment();
+	}
 
 
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+							 @Nullable Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRetainInstance(true);
+		mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.Photo_recycler_view);
+		mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
 
-    }
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+
+
+	}
 }
