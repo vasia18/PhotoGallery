4232820fd46bbf01010303a31afd9cad5a179584
vasia18 <studiovasia18@gmail.com>
2021-02-01 01:42:32 +0300

    Код который запускается (Но там ничего пока нет)

--- app/src/main/java/ru/PhotoGallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/PhotoGallery/PhotoGalleryFragment.java
@@ -7,8 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,11 +25,11 @@ public class PhotoGalleryFragment extends Fragment {
 
 
 	@Override
-	public View onCreateView( LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
-		mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.Photo_recycler_view);
+		mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
 		mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
 
